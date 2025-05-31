3e6bf169d8fa93b45414cd9af5e27ebe19ca4d47
isabela figueroa <isa08.figueroa.11@gmail.com>
2025-05-31 17:25:50 -0500

    documento actualizado

---
+++ condicionales.py
@@ -0,0 +1,18 @@
+edad=int(input("Ingrese su edad: "))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+
+#sentencia elif
+num1=float(input("ingresa el primer numero a multiplicar: "))
+num2=float(input("ingrese el segundo numero a multiplicar: "))
+resultado= num1*num2
+opcion= "multipicar"
+if opcion == "sumar":
+    print("Esto es una suma")
+elif opcion == "multiplicar":
+print("El resultado de la multiplicacion es:  ",{resultado})
+
+
