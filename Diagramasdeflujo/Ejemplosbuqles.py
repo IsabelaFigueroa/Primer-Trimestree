a17b33159a8adb9a740242a61732e7192615aee1
isabela figueroa <isa08.figueroa.11@gmail.com>
2025-06-14 17:31:57 -0500

    Nuevo trabajo

---
+++ Diagramasdeflujo/Ejemplosbuqles.py
@@ -0,0 +1,12 @@
+#SENTENCIA WHILE
+contador=1
+
+while contador <= 100:
+     print("Contador: ", contador)
+contador += 1
+
+contador=1
+
+while contador <= 200:
+    print("Contador: ", contador)
+    contador += 1
